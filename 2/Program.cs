4d6ef849f6faedfeba46d8b89a5afd20b02f523f
harrison056 <costa.silva.cs.cs@gmail.com>
2023-03-28 10:30:49 -0300

    Questão 2

---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Digite valor: ");
+int valorInformado = Convert.ToInt32(Console.ReadLine());
+
+int numeroAnterior = 0;
+int numeroAtual = 1;
+int fibonacci = 0;
+
+   
+while (fibonacci < valorInformado)
+{
+    fibonacci = numeroAnterior + numeroAtual; 
+    numeroAnterior = numeroAtual; 
+    numeroAtual = fibonacci;
+}  
+       
+
+if (valorInformado == fibonacci)
+{
+    Console.WriteLine("Pertece a sequência!");
+}else
+{
+    Console.WriteLine("Não Pertece a sequência!");
+}
