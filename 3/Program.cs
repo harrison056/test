07b07cca9fdf0bec4807f14d140f69dd1d405bf3
harrison056 <costa.silva.cs.cs@gmail.com>
2023-03-30 06:54:54 -0300

    Questão 3

---
+++ 3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Newtonsoft.Json;
+
+internal class Program
+{   
+    private static void Main(string[] args)
+    {
+        string json = @"json/dados.json";
+        if(File.Exists(json))
+        {
+            int menorDia = 0, maiorDia = 0;
+            double menorValor = 0, maiorValor = 0, media = 0;
+            double soma = 0; //Soma da media
+            int div = 0;//divisão de media
+            int[] acima = new int[29];
+
+            var items = JsonConvert.DeserializeObject<List<Dias>>(File.ReadAllText(json));
+            
+            for (int i = 0; i < 30; i++)
+            {
+                if (items[i].valor > maiorValor)
+                {
+                    maiorValor = items[i].valor;
+                    maiorDia = items[i].dia;
+                }
+                if (items[i].valor <= items[29].valor && items[i].valor != 0)
+                {
+                    menorValor = items[i].valor;
+                    menorDia = items[i].dia;
+                }     
+                if (items[i].valor != 0)
+                {
+                    div ++;
+                    soma = soma + items[i].valor;
+                    media = (soma + items[i].valor) / div;
+                }    
+                if (media < items[i].valor)
+                {
+                    acima[](items[i].dia);
+                }    
+            }
+
+            Console.WriteLine("O menor valor de faturamento ocorrido em um dia do mês: Dia "+ menorDia +" - R$" + menorValor);
+            Console.WriteLine("O maior valor de faturamento ocorrido em um dia do mês: Dia "+ maiorDia +" - R$" + maiorValor);
+            Console.WriteLine(acima[1]);
+        }
+    }
+
+    public class Dias
+    {
+        public int dia { get; set; }
+        public double valor { get; set; }
+    }
+}
